9b70f525fb450a9581739b80552f4cba79df3978
asjV2 <ahmad.subaktagin16@gmail.com>
2022-05-18 21:32:52 +0600

    Tasks by User Displayed

--- src/Components/Home/SingleTask.js
+++ src/Components/Home/SingleTask.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const SingleTask = () => {
+const SingleTask = ({ task }) => {
   return (
     <tr>
-      <td>Cy Ganderton</td>
+      <td>{task.taskName}</td>
       <td className="flex flex-col gap-2">
         <button className="btn btn-sm btn-success">Complete</button>
         <button className="btn btn-sm btn-error">Delete</button>
--- src/Components/Home/TaskList.js
+++ src/Components/Home/TaskList.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "./../../firebase.init";
 import SingleTask from "./SingleTask";
 
 const TaskList = () => {
+  const [user] = useAuthState(auth);
+  const [myTasks, setMyTasks] = useState([]);
+  useEffect(() => {
+    const email = user?.email;
+    fetch(`http://localhost:5000/tasks?email=${email}`)
+      .then((res) => res.json())
+      .then((data) => setMyTasks(data));
+  }, [user]);
   return (
     <div className="overflow-x-auto my-8">
       <table className="table w-full">
@@ -12,7 +22,9 @@ const TaskList = () => {
           </tr>
         </thead>
         <tbody>
-          <SingleTask />
+          {myTasks.map((task) => (
+            <SingleTask task={task} key={task._id} />
+          ))}
         </tbody>
       </table>
     </div>
